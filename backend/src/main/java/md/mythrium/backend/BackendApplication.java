407622ff3e53efe00567aa9565aa1a3f2007d58f
Wartuu <88190400+Wartuu@users.noreply.github.com>
2023-11-21 23:58:29 +0100

    base project initialization

---
+++ backend/src/main/java/md/mythrium/backend/BackendApplication.java
@@ -0,0 +1,26 @@
+package md.mythrium.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class BackendApplication {
+
+
+
+
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello, World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
