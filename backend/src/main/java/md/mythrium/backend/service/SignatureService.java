2454cc78d2cef59a770ef7bd94ad1daa89d79c97
Wartuu <Michu4154@gmail.com>
2024-02-02 05:17:43 +0100

    updated JwtUtils.java

---
+++ backend/src/main/java/md/mythrium/backend/service/SignatureService.java
@@ -0,0 +1,14 @@
+package md.mythrium.backend.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class SignatureService {
+
+    public Key getSignature() {
+        return null; // TODO: signatures
+    }
+}
--- backend/src/main/java/md/mythrium/backend/utils/JwtUtils.java
+++ backend/src/main/java/md/mythrium/backend/utils/JwtUtils.java
@@ -1,15 +1,50 @@
 package md.mythrium.backend.utils;
 
 import io.jsonwebtoken.Jwts;
+import md.mythrium.backend.entity.account.Account;
+import md.mythrium.backend.service.SignatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-public class JwtUtils {
-    private static final String SIGNATURE = "TEST_SIGNATURE";
-    private static final long EXPIRATION = TimeUnit.DAYS.toMillis(14);
 
-    public static String generateJwt(String data) {
-        return null;
+@Service
+public class JwtUtils {
+    private final long EXPIRATION = TimeUnit.DAYS.toMillis(7);
+
+    @Autowired
+    private final SignatureService signatureService;
+
+
+    public JwtUtils(SignatureService signatureService) {
+        this.signatureService = signatureService;
+    }
+
+
+    public String generateJwt(Account account) {
+        return Jwts.builder()
+                .header()
+                .type("JWT")
+                .and()
+                .content("uuid", UUID.randomUUID().toString())
+                .content("exp", String.valueOf(System.currentTimeMillis() + EXPIRATION))
+                .content("iat", String.valueOf(System.currentTimeMillis()))
+
+                .content("email", account.getEmail())
+                .content("username", account.getUsername())
+                .claim("authority", account.getRoles())
+
+                .signWith(signatureService.getSignature())
+                .compact();
+    }
+
+    public boolean authenticateJwt(String jwt) {
+        return false;
     }
 
 }
