1d764917edbe41f319c9158eb09da6ce190176ee
Wartuu <Michu4154@gmail.com>
2024-03-21 22:58:37 +0100

    phone responsive problem fixed, MobileFallback

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,6 +4,7 @@ import './styles/global.scss'
 
 import Spinner from './components/spinner';
 import DEBUG from './routes/debug';
+import MobileFallback from './routes/mobileFallback';
 
 const HomePage      =   React.lazy(() => import('./routes/home'))
 const Error404      =   React.lazy(() => import('./routes/error404'))
@@ -49,15 +50,27 @@ const router = createBrowserRouter([
         'path': '*',
         element: <Error404/>
     }
-])  
+])
+
+function isMobileUser() {
+    const mobileRegex = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i;
+    return mobileRegex.test(navigator.userAgent);
+
+}
+
 
 const App = () => {
     return(
-        <Suspense fallback={
-            <div id="page" className="center-flex"><Spinner/></div>
-        }>
-            <RouterProvider router={router}/>
-        </Suspense> 
+        (isMobileUser() ? (
+            <MobileFallback/>
+        ) : (
+            <Suspense fallback={
+                <div id="page" className="center-flex"><Spinner/></div>
+            }>
+                <RouterProvider router={router}/>
+            </Suspense> 
+        ))
+
     )
 }
 
---
+++ frontend/src/routes/mobileFallback.jsx
@@ -0,0 +1,17 @@
+
+
+const MobileFallback = () => {
+    return (
+        <>
+            <div id="cards">
+                <div className="card">
+                    hello
+                </div>
+            </div>
+
+        </>
+
+    )
+}
+
+export default MobileFallback;
